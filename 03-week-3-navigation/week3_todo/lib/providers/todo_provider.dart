38970b7eccb4bec7182d2f8abfd235f5e1fae24f
ErilOcshada <erilleo23@gmail.com>
2026-09-09 09:45:02 +0700

    Simulate AsyncValue error state

--- 03-week-3-navigation/week3_todo/lib/providers/todo_provider.dart
+++ 03-week-3-navigation/week3_todo/lib/providers/todo_provider.dart
@@ -12,8 +12,7 @@ class Todo {
 class TodoListNotifier extends AsyncNotifier<List<String>> {
   @override
   Future<List<String>> build() async {
-    await Future.delayed(const Duration(seconds: 5)); // simulasi network
-    return ['Keyboard', 'Mouse', 'Monitor'];
+    throw Exception('Gagal terhubung ke server');
   }
 
   Future<void> refresh() async {
