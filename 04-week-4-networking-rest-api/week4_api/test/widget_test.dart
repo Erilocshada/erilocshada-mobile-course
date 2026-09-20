09843728b6106c429eabc7c263f50ce3215d5402
ErilOcshada <erilleo23@gmail.com>
2026-09-20 22:54:40 +0700

    Praktikum 3

--- 04-week-4-networking-rest-api/week4_api/test/widget_test.dart
+++ 04-week-4-networking-rest-api/week4_api/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:week4_api/main.dart';
+import 'package:week4_api/PagedPostPage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
